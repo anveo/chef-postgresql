c57e9cfa74821a5512242d1123de4aa9db665551
Brian Racer <bracer@gmail.com>
2013-04-23 12:31:45 -0600

    ubuntu specific fix for phlipper/chef-postgresql/issues/28

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,8 +5,21 @@
 
 include_recipe "postgresql"
 
+file "/usr/sbin/policy-rc.d" do
+  mode 0755
+  owner "root"
+  group "root"
+  content "#!/bin/sh\nexit 101"
+  not_if "dpkg -s postgresql-#{node["postgresql"]["version"]}"
+end
+
 # install the package
 package "postgresql-#{node["postgresql"]["version"]}"
+
+file "/usr/sbin/policy-rc.d" do
+  action :delete
+  only_if "dpkg -s postgresql-#{node["postgresql"]["version"]}"
+end
 
 # setup the data directory
 include_recipe "postgresql::data_directory"
